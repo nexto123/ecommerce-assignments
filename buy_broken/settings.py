11920f259b3ae5045fc55f48981018e668ea2b56
ernest <pristowel@gmail.com>
2019-04-18 15:31:40 +0000

    i created gitignore file and env.py file

--- buy_broken/settings.py
+++ buy_broken/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+if os.path.exists('env.py'):
+    import env
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lwb+4yw$r1i#o*wbm6u0u9t8@+ud(6+4+6puagg+xzh46fqq9&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
