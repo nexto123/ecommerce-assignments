4ad77378b770f5fffb0ac8edba4fbc087d2f0efe
ernest <pristowel@gmail.com>
2019-04-18 15:38:45 +0000

    cart urls to views created

--- buy_broken/urls.py
+++ buy_broken/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('cart/', include('cart.urls')),
     path('shop/', include('shop.urls')),
     path('admin/', admin.site.urls),
 ]
