ec2f58f2e01959240e2095b1d26fbb2667ccf9dd
ernest <pristowel@gmail.com>
2019-04-19 22:20:58 +0000

    added order template route

--- buy_broken/settings.py
+++ buy_broken/settings.py
@@ -65,7 +65,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'cart','templates/'),
                 os.path.join(BASE_DIR,'search_app','templates/'),
-                os.path.join(BASE_DIR,'shop','templates/'),],
+                os.path.join(BASE_DIR,'shop','templates/'),
+                os.path.join(BASE_DIR,'shop','templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
