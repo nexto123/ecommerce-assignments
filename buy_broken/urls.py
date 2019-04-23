df0910282bb77ff3d00ea0330bbeda5f7e875ab0
ernest <pristowel@gmail.com>
2019-04-23 23:56:28 +0000

    added index route

--- buy_broken/urls.py
+++ buy_broken/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('cart/', include('cart.urls')),
     path('shop/', include('shop.urls')),
     path('order/', include('order.urls')),
