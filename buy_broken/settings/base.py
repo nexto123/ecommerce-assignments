a3873d85b839531e8302526f09a933a622782d49
ernest <pristowel@gmail.com>
2019-04-24 15:00:59 +0000

    dep test

--- buy_broken/settings/base.py
+++ buy_broken/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ecommerce-assignment-nexto123.c9users.io', '.herokuapp.com']
 
