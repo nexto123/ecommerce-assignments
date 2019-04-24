7ef7bc8e713c1dee2dc718d18fabee5d064ea874
ernest <pristowel@gmail.com>
2019-04-24 11:29:06 +0000

    deployment to heroku test

--- buy_broken/settings.py
+++ buy_broken/settings.py
@@ -158,3 +158,19 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "postmaster@sandbox3b7b6fa3dc2b493182d2e30d5fa7e82b.mailgun.org"
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+AWS_STORAGE_BUCKET_NAME = 'myblogproject'
+AWS_S3_REGION_NAME = 'eu-west-1'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+print(STATICFILES_STORAGE)
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
