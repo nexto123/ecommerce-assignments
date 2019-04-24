c7f0330a3a1a859584f2fa77a03cd5fdd44691ff
ernest <pristowel@gmail.com>
2019-04-24 22:33:27 +0000

    dev test

--- buy_broken/settings/base.py
+++ buy_broken/settings/base.py
@@ -66,10 +66,8 @@ ROOT_URLCONF = 'buy_broken.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'cart','templates/'),
-                os.path.join(BASE_DIR,'search_app','templates/'),
-                os.path.join(BASE_DIR,'shop','templates/'),
-                os.path.join(BASE_DIR,'shop','templates/')],
+        'DIRS': [os.path.join(BASE_DIR,'templates/'),],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -141,13 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-    )
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static','media')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+ 
+     
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'static','media')
 
 STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
@@ -160,5 +157,34 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "postmaster@sandbox3b7b6fa3dc2b493182d2e30d5fa7e82b.mailgun.org"
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-from buy_broken.aws.conf import *
+
+AWS_USERNAME = 'nexto-user'
+AWS_GROUP_NAME = 'buy_broken_group'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
+
+DEFAULT_FILE_STORAGE = 'buy_broken.aws.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'buy_broken.aws.utils.StaticRootS3BotoStorage'
+
+AWS_STORAGE_BUCKET_NAME = 'buy-broken'
+S3DIRECT_REGION = 'eu-west-1'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + '/static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = { 
+    'Expires': expires,
+    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+}
+
+AWS_QUERYSTRING_AUTH = False
 
