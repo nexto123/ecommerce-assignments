324b31c8b6a19f23d239f6179196836c931e915d
ernest <pristowel@gmail.com>
2019-04-25 08:04:48 +0000

    final prep for dep

--- buy_broken/settings.py
+++ buy_broken/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'stripe',
     'order',
     'crispy_forms',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -142,9 +141,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR,'static'),
-#     )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static','media')
 
@@ -160,41 +159,41 @@ EMAIL_HOST_USER = "postmaster@sandbox3b7b6fa3dc2b493182d2e30d5fa7e82b.mailgun.or
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
-AWS_USERNAME = 'nexto-user'
-AWS_GROUP_NAME = 'buy_broken_group'
-AWS_ACCESS_KEY_ID = 'AKIA5TX2BNIQJQP5EIW3'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_FILE_EXPIRE = 200
-AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = True
+# AWS_USERNAME = 'nexto-user'
+# AWS_GROUP_NAME = 'buy_broken_group'
+# AWS_ACCESS_KEY_ID = 'AKIA5TX2BNIQJQP5EIW3'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_FILE_EXPIRE = 200
+# AWS_PRELOAD_METADATA = True
+# AWS_QUERYSTRING_AUTH = True
 
-StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
-MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
+# StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
+# MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
 
-DEFAULT_FILE_STORAGE = 'buy_broken.aws.utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'buy_broken.aws.utils.StaticRootS3BotoStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_STORAGE_BUCKET_NAME = 'buy-broken'
-S3DIRECT_REGION = 'eu-west-1'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
-STATIC_URL = S3_URL + '/static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-AWS_DEFAULT_ACL = None
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
+# AWS_STORAGE_BUCKET_NAME = 'buy-broken'
+# S3DIRECT_REGION = 'eu-west-1'
+# S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+# MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+# MEDIA_ROOT = MEDIA_URL
+# STATIC_URL = S3_URL + '/static/'
+# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-two_months = datetime.timedelta(days=61)
-date_two_months_later = datetime.date.today() + two_months
-expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
 
-AWS_HEADERS = { 
-    'Expires': expires,
-    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
-}
+# two_months = datetime.timedelta(days=61)
+# date_two_months_later = datetime.date.today() + two_months
+# expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
 
-AWS_QUERYSTRING_AUTH = False
+# AWS_HEADERS = { 
+#     'Expires': expires,
+#     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+# }
+
+# AWS_QUERYSTRING_AUTH = False
 
 
