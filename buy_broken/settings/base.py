19a2513a95d7c6e15309eb65ae5237d956b58a2b
ernest <pristowel@gmail.com>
2019-04-25 06:20:45 +0000

    set for heroku push

--- buy_broken/settings/base.py
+++ buy_broken/settings/base.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ecommerce-assignment-nexto123.c9users.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['ecommerce-assignment-nexto123.c9users.io', 'buybucket.herokuapp.com']
 
 
 # Application definition
